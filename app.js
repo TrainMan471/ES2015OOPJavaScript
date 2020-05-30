88cae11e402f8a4cf4999107ac498410bf1beca5
Michael Brown <brownmichael486@gmail.com>
2020-05-30 17:49:36 -0400

    practice with Classes

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const ernie = {
-  animal : 'dog',
-  age : '1',
-  breed : 'dog',
-  bark : function () {
-    console.log("Woof");
+class Pet {
+  constructor(animal,age,breed, sound) {
+    this.animal = animal;
+    this.age = age;
+    this.breed = breed;
+    this.sound = sound;
   }
+  speak() {
+    console.log(this.sound);
+  }
+
 }
 
-console.log(ernie.age);
-console.log(ernie.breed);
-ernie.bark();
+const hamilton = new Pet( 'dog', 2, 'maltese','woof');
+const ernie = new Pet('dog', 1, 'pug','woof');
+const rocket = new Pet('dog', 3, 'labador','woof');
+const buttercup = new Pet('cat', 12, 'tabby','meow');
+const mrsFloof = new Pet('cat', 7, 'maincoon','meow');
+const marvel = new Pet('cat', 3, 'tabby','meow');
+
+console.log(hamilton);
+console.log(ernie);
+console.log(rocket);
+console.log(buttercup);
+console.log(mrsFloof);
+console.log(marvel);
+
+hamilton.speak();
