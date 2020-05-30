e75a8e30621dbb636d3a0158a3b469062dc5e417
Michael Brown <brownmichael486@gmail.com>
2020-05-30 16:11:56 -0400

    Added server and practicing objects file

---
+++ app.js
@@ -0,0 +1,12 @@
+const ernie = {
+  animal : 'dog',
+  age : '1',
+  breed : 'dog',
+  bark : function () {
+    console.log("Woof");
+  }
+}
+
+console.log(ernie.age);
+console.log(ernie.breed);
+ernie.bark();
